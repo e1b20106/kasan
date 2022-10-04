f6be9ad969b18d4041c467df742770966d992f9f
B20107 <e1b20107@st.oit.ac.jp>
2022-10-04 14:38:55 +0900

    リポジトリの初期化

---
+++ src/main/java/oit/is/z1102/higashi/kasan/KasanApplication.java
@@ -0,0 +1,13 @@
+package oit.is.z1102.higashi.kasan;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KasanApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KasanApplication.class, args);
+	}
+
+}
---
+++ src/test/java/oit/is/z1102/higashi/kasan/KasanApplicationTests.java
@@ -0,0 +1,13 @@
+package oit.is.z1102.higashi.kasan;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KasanApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
